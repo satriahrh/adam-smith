96a4479905cb8ea51513245e9bb9a4be4e9bd8f7
Satria H R Harsono <satriahrh@gmail.com>
2020-12-13 20:44:32 +0700

    product detail struct

--- ent/schema/product.go
+++ ent/schema/product.go
@@ -11,24 +11,32 @@ type Product struct {
 	ent.Schema
 }
 
+// ProductDescription description of a product
+type ProductDescription struct {
+	Heading string `json:"heading"`
+	Body    string `json:"body"`
+}
+
+// ProductMarketplace marketplace details of the product being sold
+type ProductMarketplace struct {
+	Tokopedia string `json:"tokopedia"`
+	Shopee    string `json:"shopee"`
+	Bukalapak string `json:"bukalapak"`
+}
+
+// ProductImages detail images of the product
+type ProductImages struct {
+	Thumbnail string   `json:"thumbnail"`
+	Displays  []string `json:"displays"`
+}
+
 // Fields of the Product.
 func (Product) Fields() []ent.Field {
 	return []ent.Field{
 		field.String("name"),
-		field.JSON("descriptions", []struct {
-			Heading string `json:"heading"`
-			Body    string `json:"body"`
-		}{}),
-		field.JSON("images", struct {
-			Thumbnail string   `json:"thumbnail"`
-			Displays  []string `json:"displays"`
-			Variants  []string `json:"variants"`
-		}{}),
-		field.JSON("marketplaces", struct {
-			Tokopedia string `json:"tokopedia"`
-			Shopee    string `json:"shopee"`
-			Bukalapak string `json:"bukalapak"`
-		}{}),
+		field.JSON("descriptions", []ProductDescription{}),
+		field.JSON("images", ProductImages{}),
+		field.JSON("marketplaces", ProductMarketplace{}),
 	}
 }
 
