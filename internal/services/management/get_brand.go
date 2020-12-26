9dc0afda9750e3291eea4ffc5b7b3135c64dc0bf
Satria H R Harsono <satriahrh@gmail.com>
2020-12-27 00:01:46 +0700

    get brand

---
+++ internal/services/management/get_brand.go
@@ -0,0 +1,46 @@
+package management
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/satriahrh/adam-smith/build/ent"
+	"github.com/satriahrh/adam-smith/build/ent/brand"
+	"github.com/satriahrh/adam-smith/build/proto"
+	"github.com/satriahrh/adam-smith/internal/helper"
+	"go.uber.org/zap"
+)
+
+// GetBrand get single brand by ID or by Code. Use one of them but not both.
+func (m *management) GetBrand(ctx context.Context, brandID uint64, brandCode string) (protoBrand *proto.Brand, err error) {
+	defer func() {
+		if err != nil {
+			err = func(err error) error {
+				return helper.ErrorParseFromDictionary(err, []helper.ErrorDictionaryItem{
+					{From: 0, Value: "ent: brand not found", Error: fmt.Errorf("no brand found with given id/code")},
+				})
+			}(err)
+		}
+	}()
+
+	var retrievedBrand *ent.Brand
+	if brandID != 0 {
+		if retrievedBrand, err = m.ent.Brand.Get(ctx, brandID); err != nil {
+			m.logger.Error("Error getting brand by ID", zap.Any("action_id", ctx.Value("ActionID")), zap.Error(err))
+		}
+	} else {
+		if retrievedBrand, err = m.ent.Brand.Query().Where(brand.CodeEQ(brandCode)).First(ctx); err != nil {
+			m.logger.Error("Error getting brand by Code", zap.Any("action_id", ctx.Value("ActionID")), zap.Error(err))
+		}
+	}
+
+	if err == nil {
+		protoBrand = &proto.Brand{
+			Id:   retrievedBrand.ID,
+			Code: retrievedBrand.Code,
+			Name: retrievedBrand.Name,
+		}
+	}
+
+	return
+}
---
+++ internal/services/management/get_brand_test.go
@@ -0,0 +1,57 @@
+package management_test
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/satriahrh/adam-smith/build/proto"
+)
+
+func (s *Suite) TestGetBrand() {
+	s.Run("ByID", func() {
+		s.Run("Error", func() {
+			s.sqlmock.ExpectQuery("SELECT (.+) FROM `brands`").
+				WithArgs(123).
+				WillReturnRows(sqlmock.NewRows([]string{"id", "code", "name"}))
+			_, err := s.subject.GetBrand(s.context, 123, "")
+			s.EqualError(err, "no brand found with given id/code")
+		})
+		s.Run("Succeed", func() {
+			s.sqlmock.ExpectQuery("SELECT (.+) FROM `brands`").
+				WithArgs("savan").
+				WillReturnRows(
+					sqlmock.NewRows([]string{"id", "code", "name"}).
+						AddRow(uint64(123), "savan", "Savan Baby Wear"),
+				)
+			protoBrand, err := s.subject.GetBrand(s.context, 0, "savan")
+			s.NoError(err)
+			s.EqualValues(&proto.Brand{
+				Id:   123,
+				Code: "savan",
+				Name: "Savan Baby Wear",
+			}, protoBrand)
+		})
+	})
+	s.Run("ByCode", func() {
+		s.Run("Error", func() {
+			s.sqlmock.ExpectQuery("SELECT (.+) FROM `brands`").
+				WithArgs("savan").
+				WillReturnRows(sqlmock.NewRows([]string{"id", "code", "name"}))
+			_, err := s.subject.GetBrand(s.context, 0, "savan")
+			s.EqualError(err, "no brand found with given id/code")
+		})
+		s.Run("Succeed", func() {
+			s.sqlmock.ExpectQuery("SELECT (.+) FROM `brands`").
+				WithArgs("savan").
+				WillReturnRows(
+					sqlmock.NewRows([]string{"id", "code", "name"}).
+						AddRow(uint64(123), "savan", "Savan Baby Wear"),
+				)
+			protoBrand, err := s.subject.GetBrand(s.context, 0, "savan")
+			s.NoError(err)
+			s.EqualValues(&proto.Brand{
+				Id:   123,
+				Code: "savan",
+				Name: "Savan Baby Wear",
+			}, protoBrand)
+		})
+	})
+}
